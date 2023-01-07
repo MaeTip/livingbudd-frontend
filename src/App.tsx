dff4a2c5d442fd64b1088ceb3c3bc60db9d86d2d
Numpon Maytangsuphut <numpon014@gmail.com>
2023-01-07 21:36:25 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import livingBuddLogo from './living_budd_logo.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={livingBuddLogo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
