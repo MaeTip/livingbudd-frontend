ea8daeecee53f813f8c6149f061005ded001e0ce
Numpon Maytangsuphut <numpon014@gmail.com>
2023-01-23 12:40:23 +0700

    updated table of reservation

--- src/containers/pages/admin/ReservationList/index.styles.tsx
+++ src/containers/pages/admin/ReservationList/index.styles.tsx
@@ -2,4 +2,11 @@ import styled from 'styled-components'
 
 export const PageWrapper = styled.div`
   padding: 0 25px;
+  
+  .actions {
+    text-align: center;
+    a {
+      padding-right: 10px;
+    }
+  }
 `
--- src/containers/pages/admin/ReservationList/index.tsx
+++ src/containers/pages/admin/ReservationList/index.tsx
@@ -3,11 +3,19 @@ import { PageWrapper } from "./index.styles";
 import { useGetAllReservationsQuery } from "redux/api/reservation.api";
 import { toast } from "react-toastify";
 import React, { useEffect } from "react";
-import { Table } from "antd";
+import { message, Modal, Table } from "antd";
 import Title from "antd/es/typography/Title";
-import moment from 'moment';
+import moment from "moment";
+import {
+  CheckOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  ExclamationCircleFilled,
+} from "@ant-design/icons";
+import { IReservation } from "redux/dto/reservation.dto";
 
 const ReservationListPage = () => {
+  const { confirm } = Modal;
   const {
     isLoading,
     isError,
@@ -32,38 +40,74 @@ const ReservationListPage = () => {
     }
   }, [isLoading]);
 
-  const columns : any = [
+  const onClickedDeleteButton = (data: IReservation): void => {
+    confirm({
+      title: t("common.confirm.delete"),
+      icon: <ExclamationCircleFilled />,
+      content: (
+        <div>
+          <div>
+            {t("reservation.data.fullname")} : {data.fullname}
+          </div>
+          <div>
+            {t("reservation.data.phone")} : {data.phone}
+          </div>
+        </div>
+      ),
+      onOk() {
+        console.log("OK");
+      }
+    });
+  };
+
+  const onClickedEditButton = (data: IReservation) => {
+    message.info(`click edit for ${data.fullname}`);
+  };
+
+  const columns: any = [
     {
-      title: 'Action',
-      key: 'operation',
+      title: "Action",
+      key: "operation",
       width: 80,
-      fixed: 'left',
-      responsive: ['md'],
-      render: () => <a>action</a>,
+      fixed: "left",
+      responsive: ["md"],
+      render: (record: IReservation) => (
+        <div className="actions">
+          <a onClick={() => onClickedEditButton(record)}>
+            <EditOutlined />
+          </a>
+          <a onClick={() => onClickedDeleteButton(record)}>
+            <DeleteOutlined />
+          </a>
+        </div>
+      ),
     },
     {
       title: t("reservation.data.created_at"),
       dataIndex: "createdAt",
       key: "createdAt",
       width: 120,
-      fixed: 'left',
-      defaultSortOrder: 'descend',
-      sorter: (a: any, b: any) => moment(new Date(a.createdAt)).unix() - moment(new Date(b.createdAt)).unix(),
-      render: (createdAt: string) => moment(new Date(createdAt)).format('DD/MM/YYYY'),
+      fixed: "left",
+      defaultSortOrder: "descend",
+      sorter: (a: any, b: any) =>
+        moment(new Date(a.createdAt)).unix() -
+        moment(new Date(b.createdAt)).unix(),
+      render: (createdAt: string) =>
+        moment(new Date(createdAt)).format("DD/MM/YYYY"),
     },
     {
       title: t("reservation.data.fullname"),
       dataIndex: "fullname",
       key: "fullname",
       width: 200,
-      fixed: 'left',
+      fixed: "left",
     },
     {
       title: t("reservation.data.phone"),
       dataIndex: "phone",
       key: "phone",
       width: 120,
-      fixed: 'left',
+      fixed: "left",
     },
     {
       title: t("reservation.data.email"),
@@ -76,42 +120,45 @@ const ReservationListPage = () => {
       dataIndex: "age",
       key: "age",
       width: 80,
-      responsive: ['md'],
+      responsive: ["md"],
     },
     {
       title: t("reservation.data.gender"),
       dataIndex: "gender",
       key: "gender",
       width: 80,
-      responsive: ['md'],
-      render: (value: string) => t(`reservation.data.option.gender.${value.toLowerCase()}`),
+      responsive: ["md"],
+      render: (value: string) =>
+        t(`reservation.data.option.gender.${value.toLowerCase()}`),
     },
     {
       title: t("reservation.table_columns.number_of_tenant"),
       dataIndex: "number_of_tenant",
       key: "number_of_tenant",
       width: 100,
-      responsive: ['md'],
+      responsive: ["md"],
     },
     {
       title: t("reservation.table_columns.pet_required"),
-      dataIndex: "pet_required",
-      key: "pet_required",
+      dataIndex: "has_pet",
+      key: "has_pet",
       width: 100,
-      responsive: ['md']
+      responsive: ["md"],
+      render: (value: boolean) => (value ? <CheckOutlined /> : ""),
     },
     {
       title: t("reservation.table_columns.vehicle"),
       dataIndex: "vehicle",
       key: "vehicle",
       width: 150,
-      responsive: ['md'],
-      render: (value: string) => t(`reservation.data.option.vehicle.${value.toLowerCase()}`),
+      responsive: ["md"],
+      render: (value: string) =>
+        t(`reservation.data.option.vehicle.${value.toLowerCase()}`),
     },
     {
       title: t("reservation.data.additional_request"),
       dataIndex: "additional_request",
-      responsive: ['md'],
+      responsive: ["md"],
       key: "additional_request",
       width: 200,
     },
@@ -120,9 +167,8 @@ const ReservationListPage = () => {
       dataIndex: "contact",
       key: "contact",
       width: 200,
-      responsive: ['md'],
-    }
-
+      responsive: ["md"],
+    },
   ];
 
   return (
@@ -132,7 +178,11 @@ const ReservationListPage = () => {
       ) : (
         <div>
           <Title>ลงทะเบียนจองห้องพัก</Title>
-          <Table columns={columns} dataSource={reservations} scroll={{ y: 'calc(100vh) - 200px' }} />
+          <Table
+            columns={columns}
+            dataSource={reservations}
+            scroll={{ y: "calc(100vh) - 200px" }}
+          />
         </div>
       )}
     </PageWrapper>
